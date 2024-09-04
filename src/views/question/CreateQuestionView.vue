615d291187f6673503e04e273c0770bd19908f8a
Titi <levendeurlillois@hotmail.fr>
2024-09-04 16:14:20 +0200

    avancement sur le quiz : questions, reponses du quiz fait : TODO radio input avec bonne réponse

--- src/views/question/CreateQuestionView.vue
+++ src/views/question/CreateQuestionView.vue
@@ -1,58 +1,211 @@
 <template>
 
-    <h1>créer de nouvelles questions - Quiz : {{ quiz }}</h1>
+  <h1>créer de nouvelles questions - Quiz : {{ quiz }}</h1>
     <!--action="http://localhost:3020/api/question/newQuestion"-->
 
-      <div v-for="count in totalCount">
-        <div :style="{display: count == totalCount ? 'block' : 'none'}">
-          <p>question {{count}}</p>
-          <input ref="inputTest" type="text" :name="`question${count}`" ><br>
-        </div>
+  <section class="quiz-container">
 
-<!--     <div v-for="answer in totalAnswer">
-          <label for="answer1">Réponse 1:</label>
-          <input type="text" name="answer1"></input>
-          <input type="radio" :name="`question${count}`">
-        </div>
-        
-        <label for="answer2">Réponse 2:</label>
-        <input type="text" name="answer2"></input>
-        <input type="radio" value="">
-        
-        <label for="answer3">Réponse 3:</label>
-        <input type="text" name="answer3"></input>
-        <input type="radio" :name="`question${count}`">
-        
-        <label for="answer4">Réponse 4:</label>
-        <input type="text" name="answer4"></input>
-        <input type="radio" :name="`question${count}`">
-        </div>
-      -->  
+    
+    
 
+
+    <div v-for="count in totalCount" :key="numberQuestion">
+      <form action="" @submit.prevent="nextQuestion">
+
+      <div :style="{display: count == numberQuestion ? 'block' : 'none'}">
+        <p>question {{count}}</p>
+        <input  v-model="inputTest"  type="text" :name="`question${count}`" ><br>
+        <button >ajouter une autre question</button>
+
+
+        <div v-for="(answer,i) in totalAnswer" :key="i">
+          <label :for="`answer${answer}`">Réponse 1:</label>
+          <input v-model="answerTest[i]" type="text" name="answer">
+          <input @click="miou" type="radio" name="answer" :value="answerTest[i]">
+          <button type="button" @click="duplicateAnswer">ajouter une réponse</button>
+          <button type="button" @click="reduceAnswer(i)">retirer la réponse</button>
+        </div>
       </div>
+      
+
+
+      </form>
+    </div>
+
+
+
+
+    <div v-for="d in data">
+      <p>le numéro de la question : {{d.id}}</p>
+      <p> la question :{{d.question}}</p>
+      <p>bonne réponse :{{d.goodAnswer}}</p>
+      <div v-for="(a,i) in d.answer">
+        <p>
+         les réponses {{ a }}
+        </p>
+      </div>
+      
+    </div>
+    
+      
+
+  </section>
+
+
+
+      
       
       <button @click="data" type="submit">Sauvegarder le quiz</button>
 
     <button @click="previewQuestion">Revenir à la question précédente</button>
     <!--<button @click="duplicateQuestion">Ajouter une question</button>-->
-    <button @click="nextQuestion">ajouter une autre question</button>
     
 </template>
 
 <script setup>
-import { ref } from 'vue';
-//import Question from './../../components/Question.vue'
-//import Answers from '@/components/Answers.vue';
+import { computed, ref } from 'vue';
 
-const test = ref('')
+const inputTest = ref('')
+const answerTest = ref([])
+const totalCount = ref(1);
+const totalAnswer = ref(1)
+const correctAnswer = ref("")
+const numberQuestion = ref(totalCount.value);
 
-const inputTest = ref(null)
 
 const quiz = ref('')
-const totalAnswer = ref(1)
-const totalCount = ref(1);
 const data = ref([]);
 const id = window.location.href.slice(48);
+
+function miou (e){
+  console.log(e);
+  
+}
+
+function duplicateAnswer () {
+totalAnswer.value++
+}
+
+function reduceAnswer (i) {
+
+  answerTest.value.splice(i,1)
+  totalAnswer.value--
+
+  console.log("index:", i, "totalans:", answerTest.value);
+  
+  //answerTest.value.slice(i-1, 1)
+}
+
+
+
+  const nextQuestion = () => {
+
+
+
+console.log("ca: ", correctAnswer.value);
+
+  
+    let answers = [];
+    answerTest.value.forEach(element => {
+      answers.push(element)
+      console.log(answers);
+    });
+
+    
+
+      data.value.splice(totalCount.value-1, 1, {
+        question: inputTest.value,
+        id: totalCount.value,
+        answer: answers,
+        goodAnswer: correctAnswer.value
+      })
+
+    /*if (data.value[numberQuestion.value].question != undefined){
+      inputTest.value = data.value[numberQuestion.value].question;
+    }*/
+
+    totalCount.value++
+    numberQuestion.value++
+    totalAnswer.value = 1
+    answerTest.value = []
+    console.log("numberquestion :", numberQuestion.value);
+    try{
+      inputTest.value = data.value[numberQuestion.value-1].question
+    }
+    catch{
+      inputTest.value = ''
+      //answerTest.value = ''
+    }
+
+
+    try{
+    totalAnswer.value = 0
+    const a = []
+    data.value[numberQuestion.value-1].answer.forEach(element => {
+      a.push(element)
+      answerTest.value = a
+      totalAnswer.value++
+    })
+  }
+  catch{
+    totalAnswer.value = 1
+  }
+    
+    //console.log("finaltest:", data.value[0].answer[0]);
+    //inputTest.value = '';
+  }
+
+
+function previewQuestion(){
+  
+  totalCount.value--
+  
+  
+  if(totalCount.value < 1 || numberQuestion.value < 1){
+    totalCount.value++
+    return
+  }
+  else{
+    numberQuestion.value--
+  totalAnswer.value = 1
+  
+  //inputTest.value = data.value[numberQuestion].question
+
+  console.log(
+    "totalCount: ", totalCount.value,
+    "numberQuestion :",  numberQuestion.value
+
+  );
+  
+  inputTest.value = data.value[numberQuestion.value-1].question;
+ 
+  try{
+    totalAnswer.value = 0
+    const a = []
+    data.value[numberQuestion.value-1].answer.forEach(element => {
+      a.push(element)
+      answerTest.value = a
+      totalAnswer.value++
+    })
+  }
+  catch{
+    totalAnswer.value = 1
+  }
+  }
+ 
+
+
+  /*
+  data.value[numberQuestion.value-1].answer.forEach(element => {
+    answerTest.value.push(element)
+    totalAnswer.value++
+  });
+*/
+}
+
+
+
+
 
 const options = {
   method: 'GET',
@@ -79,59 +232,14 @@ function handleFetch(response)
         console.error(response.statusText);
     }
   }
-
-
-
-function duplicateQuestion () {
-  
-}
-
-let a = [];
-
-function nextQuestion(e){
-  e.preventDefault();
-
-  //a.push(inputTest.value)
-
-  console.log("a", inputTest.value);
-  
-  
-
-  totalCount.value++
-
-
-//let myForm = document.getElementById("myForm");
-//let formData = new FormData(myForm);
-//console.log(formData);
-
-//data.value.push(formData);
-//console.log(data.value);
-
-
-/*
-*/
-
-/*
-  data.value.push[test.value];
-  console.log(data.value);
-  totalCount.value++
-*/
-}
-
-function previewQuestion(){
-  if(totalCount.value == 1){
-    return
-  }
-  totalCount.value--
-}
-
-
-
 </script>
 
 <style scoped>
 
-
+.quiz-container{
+  display: flex;
+  justify-content: space-evenly;
+}
 
 </style>
 
